1f7368bf1ea0da11237d59e29bcce7f080cf141e
Dmitry Sotnikov <dsotnikov1996@gmail.com>
2023-10-22 20:13:41 +0300

    remove bearer

--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -34,7 +34,6 @@ public class LoginModel : PageModel
 };
             var client = new RestClient(options);
             var request = new RestSharp.RestRequest("/api/Person/test?Email="+person.Email+"&Password="+person.Password, Method.Post);
-            request.AddHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiRG1pdHJpeSIsImxldmVsIjoiMTIzIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiQWRtaW4iLCJuYmYiOjE2OTczODU3ODgsImV4cCI6MTY5NzM4NjM4OCwiaXNzIjoiUnViaSIsImF1ZCI6InJ1YmkuZGV2In0.S_E8mDbKsmLd0WoCY4-Msi57QdLorwD4bPQHxgS8j_0");
             var response = client.Execute(request).Content;
             Console.WriteLine(response);
             Console.WriteLine(response);
